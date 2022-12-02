1c146b07e821d63826c4b7dc8b685d10baa4524d
Jack Mocherman <jackmocherman@Jacks-MBP.attlocal.net>
2022-12-02 12:42:42 -0600

    use single mapper and rotate leaf vals programatically for part 2

--- src/day2.clj
+++ src/day2.clj
@@ -4,19 +4,21 @@
 (def sample "A Y\nB X\nC Z")
 (def day2-input (slurp "./src/day2-input.txt"))
 (def mapper
-  {1 {\A {\X 4 \Y 8 \Z 3}
-      \B {\X 1 \Y 5 \Z 9}
-      \C {\X 7 \Y 2 \Z 6}}
-   2 {\A {\X 3 \Y 4 \Z 8}
-      \B {\X 1 \Y 5 \Z 9}
-      \C {\X 2 \Y 6 \Z 7}}})
+  {\A {\X 4 \Y 8 \Z 3}
+   \B {\X 1 \Y 5 \Z 9}
+   \C {\X 7 \Y 2 \Z 6}})
+
+(def rotated-mapper
+  (let [rotate-n #(->> (vals %2) cycle (drop %1) (zipmap (keys %2)))]
+    (-> mapper
+        (update \A (partial rotate-n 2))
+        (update \C (partial rotate-n 1)))))
 
 (defn determine-total-score
   [input-data part]
   (transduce
     (comp (map (juxt first last))
-          (map (partial cons part))
-          (map (partial get-in mapper)))
+          (map (partial get-in (if (= 1 part) mapper rotated-mapper))))
     +
     (str/split-lines input-data)))
 
